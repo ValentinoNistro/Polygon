de7476e81964672a75a01e2fe22407474a2098e4
ValentinoNis <210646@student.glu.nl>
2022-03-24 09:10:12 +0100

    game2

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public GameObject txtToDisplay;             //display the UI text
+
+    private bool PlayerInZone;                  //check if the player is in trigger
+
+    public GameObject Plank;
+
+    private void Start()
+    {
+
+        PlayerInZone = false;                   //player not in zone       
+        txtToDisplay.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (PlayerInZone && Input.GetKeyDown(KeyCode.E))           //if in zone and press F key
+        {
+            Plank.SetActive(!Plank.activeSelf);
+            gameObject.GetComponent<AudioSource>().Play();
+            gameObject.GetComponent<Animator>().Play("switch");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")     //if player in zone
+        {
+            txtToDisplay.SetActive(true);
+            PlayerInZone = true;
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)     //if player exit zone
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerInZone = false;
+            txtToDisplay.SetActive(false);
+        }
+    }
+}
